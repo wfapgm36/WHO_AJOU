7fcdc505f5a319ea605fe882c97c3f4efd64a178
ChanYoung Park <pgd0919@gmail.com>
2018-11-12 19:52:59 +0900

    cadd class

---
+++ team6/models/class.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+
+var classSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    professor: { type: String, required: true},
+    description: String,
+    semester: { type: String, required: true},
+    classId: String,
+    type: Number, // 1: 전공필수 2: 전공선택 3: 교양필수 4: 교양선택
+    totalGrade: { type: Number, default: 0 },  // 해당 과목에 해당하는 모든 강의 평가의 평균
+    evaluation: [{
+        writer: String, // username
+        grade1: [{
+            grade: Number,
+            memo: String
+        }],
+        grade2: [{
+            grade: Number,
+            memo: String
+        }],
+        grade3: [{
+            grade: Number,
+            memo: String
+        }],
+        totalGrade: Number, // 강의 평가 각 항목의 총 평균
+        createAt: { type: Date, default: Date.now }
+    }],
+    createAt: { type: Date, default: Date.now },
+    updated: [{ contents: String, date: { type: Date, default: Date.now } }],
+    deleted: { type: Boolean, default: false }
+});
+var Classes = mongoose.model('Classes', classSchema, 'Classlist');
+module.exports =  Classes;
