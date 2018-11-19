9768b7456e7a9df1ef6d0143447d391cf763509b
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-11-20 03:26:58 +0900

    [status] err 별 response status 값 다르게 줌

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -12,8 +12,9 @@ router.post('/', passport.authenticate('signup', {
     failWithError: true
 }), function (req, res, next) {
     res.status(200).send();
-}, function (req, res, next) {
-    res.status(404).send();
+}, function (err, req, res, next) {
+    if(err.code == 11000) res.status(202).send() // id duplication check
+    else res.status(204).send(); // email duplication
 });
 
 module.exports = router;
