0947c2fdb5a3cec22e8cb1e4405dfcd2086e381d
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-11-20 16:06:26 +0900

    [stauts] response status 수정
    passport done method 의 done(null) 호출시 401 error로 인해
    error name으로 각 케이스를 구분할 수 있도록함 -- 수정가능하다면 수정할 예정

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -13,8 +13,10 @@ router.post('/', passport.authenticate('signup', {
 }), function (req, res, next) {
     res.status(200).send();
 }, function (err, req, res, next) {
-    if(err.code == 11000) res.status(202).send() // id duplication check
-    else res.status(204).send(); // email duplication
+    if (err.code == 11000) res.status(202).send() // id duplication check
+    else if (err.toString() == "email_duplication") res.status(204).send(); // email duplication
+    else if (err.toString() == "success") res.status(200).send(); // success
+    else res.status(401).send();
 });
 
 module.exports = router;
