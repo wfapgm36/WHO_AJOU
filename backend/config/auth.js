97ff6b92d96840aaee0c7b139d39e967749774dc
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-11-21 21:41:52 +0900

    [upload] Jsonwebtoken 용 auth.js 추가

---
+++ backend/config/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const key = "token secret";
+const expiresIn = 30 * 60; // 30 minutes
+
+const auth = {
+    signToken(username) {
+        return jwt.sign({username:username}, key, {expiresIn})
+    },
+    ensureAuth() {
+        return (req, res, next) => {
+            const {authorization} = req.headers;
+            if (!authorization) {
+                res.status(401);
+                throw Error('No Authorization headers')
+            }
+            try {
+                req.user = this.verify(authorization);
+            } catch (e) {
+                res.status(401);
+                throw e
+            }
+
+            next()
+        }
+    },
+    verify(token) {
+        return jwt.verify(token.replace(/^Bearer\s/, ''), key)
+    }
+};
+
+module.exports = auth;
