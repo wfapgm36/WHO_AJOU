ea9c149d3f9fb113f1310284a4a04392554b0eb4
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-11-22 21:21:29 +0900

    [CREATE] session 유지를 위한 localstorage 저장용 store.js

---
+++ frontend/src/store/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+const resourceHost = 'http://127.0.0.1:8080/api'
+
+const enhanceAccessToken = () => {
+  const {accessToken} = localStorage
+  if (!accessToken) return
+  axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+}
+enhanceAccessToken()
+export default new Vuex.Store({
+  state: {
+    accessToken: null
+  },
+  getters: {
+
+  },
+  mutations: {
+    LOGIN (state, {accessToken}) {
+      state.accessToken = accessToken
+      localStorage.accessToken = accessToken
+    },
+    LOGOUT (state) {
+      state.accessToken = null
+      delete localStorage.accessToken
+    }
+  },
+  actions: {
+    LOGIN ({commit}, {username, password}) {
+      return axios.post(`${resourceHost}/login`, {username: username, password: password})
+        .then((res) => {
+          let data = res.data
+          commit('LOGIN', data)
+          if (res.status == '200') {
+            alert("로그인 되었습니다.")
+          } else if (res.status == '203') {
+            alert("올바르지 않은 비밀번호 입니다.")
+          } else if (res.status == "204") {
+            alert("올바르지 않은 아이디 입니다.")
+          }
+          axios.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`
+        })
+    },
+    LOGOUT ({commit}) {
+      axios.defaults.headers.common['Authorization'] = undefined
+      commit('LOGOUT')
+    },
+    CHECK () {
+      if (localStorage.accessToken != null) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+})
