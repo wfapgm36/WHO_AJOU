9986555f8485618085030122ca3d570ee88caa71
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-11-22 21:19:03 +0900

    [update] 경로 수정

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     // proxyTable {},
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://localhost:3000/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
