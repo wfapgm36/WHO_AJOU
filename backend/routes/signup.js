1a3927ba41a154d73879d8ad42d3ac34c36b890d
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-11-22 21:28:49 +0900

    [update] passport 삭제로 인한 update

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,22 +1,56 @@
 var express = require("express");
-var passport = require("passport");
 var router = express.Router();
+var User = require("../models/user");
+var mongoose = require("mongoose");
+var nev = require('email-verification')(mongoose);
 
 router.use(function (req, res, next) {
-    console.log(req.user)
-    res.locals.currentUser = req.user;
     next();
 });
 
-router.post('/', passport.authenticate('signup', {
-    failWithError: true
-}), function (req, res, next) {
-    res.status(200).send();
-}, function (err, req, res, next) {
-    if (err.code == 11000) res.status(202).send() // id duplication check
-    else if (err.toString() == "email_duplication") res.status(204).send(); // email duplication
-    else if (err.toString() == "success") res.status(200).send(); // success
-    else res.status(401).send();
+router.post('/', function (req, res, next) {
+    let newUser = new User({
+        username: req.body.username,
+        nickname: req.body.nickname,
+        password: req.body.password,
+        email: req.body.email,
+        major: req.body.major,
+        isAdmin: 0
+    });
+    User.findOne({username: req.body.username})
+        .then(data => {
+            if (data) {
+                res.status(202).send() // id duplication check
+            }
+            User.findOne({email: req.body.email})
+                .then(data => {
+                    if (data) {
+                        res.status(204).send(); // email duplication
+                    }
+                    nev.createTempUser(newUser, function (err, existingPersistentUser, newTempUser) {
+                        if (err) {
+                            console.error(err);
+                            res.send(err);
+                        }
+                        if (existingPersistentUser) {
+                            res.status(202).send() // id duplication check
+                        } else {
+                            if (newTempUser) {
+                                var URL = newTempUser[nev.options.URLFieldName];
+                                nev.sendVerificationEmail(req.body.email, URL, function (err, info) {
+                                    if (err) {
+                                        console.error(err);
+                                        res.send(err);
+                                    }
+                                    res.status(200).send(); // email duplication
+                                })
+                            } else {
+                                res.status(204).send(); // email duplication
+                            }
+                        }
+                    });
+                })
+        });
 });
 
 module.exports = router;
