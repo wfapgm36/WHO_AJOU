21b2236109fa744b05254af86dfabc996cc8ed46
ChanYoung Park <pgd0919@gmail.com>
2018-11-28 21:28:24 +0900

    댓글 삭제

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -1,7 +1,6 @@
 var express = require("express");
-var Board = require("../models/board")
+var Board= require("../models/board")
 var router = express.Router();
-const auth = require('../config/auth');
 
 router.use(function (req, res, next) {
     next();
@@ -39,38 +38,16 @@ router.get("/view/:id", function (req, res) {
     })
 });
 
-router.get("/posts/:id", auth.ensureAuth(), function (req, res) {
-    Board.findOne({_id: req.params.id})
-        .then(board => {
-            console.log(board)
-            if (board) {
-                if (board.userId == req.user.username) {
-                    res.status(200)
-                    res.json(board)
-                } else {
-                    res.status(203).send(); // writer 와 사용자 일치 x
-                }
-            } else {
-                res.status(405).send(); // data 없음
-            }
-
-        })
-});
-
-router.post("/", auth.ensureAuth(), (req, res) => {
-        let newBoard = new Board({
-            userId: req.user.username,
-            title: req.body.title,
-            writer: req.body.nickname,
-            contents: req.body.contents
-        });
-        newBoard.save();
-        res.status(200).send(Board)
+router.post("/", (req, res) => {
+    let newBoard = new Board({
+        title: req.body.title,
+        writer: req.body.nickname,
+        contents: req.body.contents
+    });
+    newBoard.save();
+    res.status(200).send(Board)
     }
 );
-
-router.get("/view/:id", function (req, res) {
-});
 
 router.post("/comment", (req, res) => {
     let comment = {
@@ -80,58 +57,35 @@ router.post("/comment", (req, res) => {
     }
     comment = req.body
     console.log(comment)
-    Board.findOne({_id: req.body.boardId}, function (err, board) {
+    Board.findOne({_id: req.body.boardId}, function(err, board){
         console.log(board)
         board.comments.push(comment)
         board.save();
-        res.status(200).send();
     })
 });
 
-router.put("/:id", auth.ensureAuth(), function (req, res) {
-    Board.findOne({_id: req.params.id})
-        .then(board => {
-            console.log(board)
-            if (board) {
-                if (board.userId == req.user.username) {
-                    Board.findOneAndUpdate({_id: req.params.id},
-                        {title: req.body.title,
-                        contents: req.body.contents} ,function (err) {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            res.status(200).send();
-                        }
-                    })
-                } else {
-                    res.status(203).send(); // writer 와 사용자 일치 x
-                }
-            } else {
-                res.status(405).send();
+router.delete("/comment", (req, res) => {
+    Board.findOne({_id: req.body.boardId}, function(err, board){
+        for(i = 0; i<board.comments.length; i++){
+            if(board.comments[i]._id == req.body.commentId){
+                console.log(board.comments[i]);
+                board.comments.remove(board.comments[i])
             }
-        })
-});
+        }
+        board.save()
+    })
+})
 
-router.delete("/:id", auth.ensureAuth(), function (req, res) {
-    Board.findOne({_id: req.params.id})
-        .then(board => {
-            if (board) {
-                if (board.userId == req.user.username) {
-                    Board.deleteOne({_id: req.params.id}, function (err) {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            res.status(200).send();
-                        }
-                    })
-                } else {
-                    res.status(203).send(); // writer 와 사용자 일치 x
-                }
-            } else {
-                res.status(405).send();
-            }
-        })
-});
+router.put("/:id", function(req, res){
+    
+})
 
+router.delete("/post/:id", function(req, res){
+    Board.deleteOne({_id: req.params.id}, function(err){
+        if(err){
+            console.log(err)
+        }
+    })
+});
 
 module.exports = router;
